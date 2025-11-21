4ca5e4db8209ade4213f65f79c8b460d1c237b77
CAP J Angga Wijaya <j.angga@ciptaartha.com>
2025-11-21 11:00:23 +0700

    tes ngrok

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ Route::get('/dtsen/report/tabulate-data', [ReportController::class, 'getTabulate
     ->name('report.tabulate');
 
 Route::get('/dtsen/proxy', function () {
-    $response = Http::get('http://simzat.kemenag.go.id/dtsen/report/tabulate-data');
+    $response = Http::get(' https://0397cd51b685.ngrok-free.app/dtsen/report/tabulate-data');
     return response()->json($response->json());
 });
