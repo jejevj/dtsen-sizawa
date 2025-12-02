7b728ae7552c4b9f718d52c166155e3e59b0b47e
CAP J Angga Wijaya <j.angga@ciptaartha.com>
2025-12-02 14:11:46 +0700

    replace title of alert

--- resources/views/layouts/partials/_scripts.blade.php
+++ resources/views/layouts/partials/_scripts.blade.php
@@ -114,7 +114,7 @@
             <!--begin::Wrapper-->
             <div class="d-flex flex-column text-light pe-0 pe-sm-10">
                 <!--begin::Title-->
-                <h4 class="mb-2  text-white">${message}</h4>
+                <h4 class="mb-2  text-white">${title}</h4>
                 <!--end::Title-->
 
                 <!--begin::Content-->
